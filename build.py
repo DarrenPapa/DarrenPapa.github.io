fe76f6f3ff55b24efe889c6bc6ecdb83ac93fbe3
DarrenPapa <darrenchasepapa@gmail.com>
2025-04-28 02:50:59 +0800

    push

--- build.py
+++ build.py
@@ -5,5 +5,6 @@ with open("lib_template.html") as input:
         res = []
         for i in os.listdir("libs"):
             lib, name = i.split(".", 1)
+            name, _ = name.rsplit(".", 1)
             res.append(f'<a href="./libs/{i}">{lib}/{name}</a><br>')
         output.write(input.read().replace("<!--FILES-->", "\n"+"\n".join(res)+"\n"))
